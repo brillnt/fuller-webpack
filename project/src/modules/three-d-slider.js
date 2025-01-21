72afb6f12f940a10750b71673ec44d23f3289a55
Dennis Porter Jr <dennisporterjr@dennismars.local>
2025-01-21 03:16:17 -0500

    new hotness

--- project/src/modules/three-d-slider.js
+++ project/src/modules/three-d-slider.js
@@ -1,4 +1,5 @@
 import { select, selectId } from '../utils/helpers.js';
+import { gsap } from '../utils/animation.js';
 import './three-d-slider.css';
 
 export default class ThreeDSlider {
@@ -7,15 +8,42 @@ export default class ThreeDSlider {
 
     this.element = selectId(elementId);
     this.indicators = this.element.querySelectorAll('.tds-indicators .tds-indicator');
+    this.slides = this.element.querySelectorAll('.tds-slides .tds-slide');
+    this.slides = Array.from(this.slides).reverse();
     this.init();
   }
 
   init() {
-    // Initialize your module here
+    // make first indicator active
     let firstIndicator = this.indicators.item(0).querySelector('.tdsi-active');
-    console.log(firstIndicator);
     if (firstIndicator) {
       firstIndicator.classList.add('tds--active');
     } 
+
+    this.indicators.forEach((indicator, index) => {
+      
+      indicator.addEventListener('click', () => {
+        this.indicators.forEach(ind => ind.querySelector('.tdsi-active').classList.remove('tds--active'));
+        indicator.querySelector('.tdsi-active').classList.add('tds--active');
+
+        gsap.to(this.slides, {
+          duration: 1,
+          ease: 'power3.inOut',
+          opacity: 0,
+          y: -50,
+          stagger: 0.1,
+          onComplete: () => {
+            this.element.querySelector('.tds-slides').innerHTML = '';
+            this.element.querySelector('.tds-slides').appendChild(this.slides[index]);
+            gsap.to(this.slides[index], {
+              duration: 1,
+              ease: 'power3.inOut',
+              opacity: 1,
+              y: 0
+            });
+          }
+        });
+      });
+    });
   }
 }
---
+++ project/src/utils/animation.js
@@ -0,0 +1,11 @@
+import { gsap } from 'gsap';
+// import { ScrollTrigger } from 'gsap/ScrollTrigger';
+// import SplitType from 'split-type';
+
+// // Register ScrollTrigger with GSAP
+// gsap.registerPlugin(ScrollTrigger);
+
+// Export everything together
+export {
+  gsap
+};
