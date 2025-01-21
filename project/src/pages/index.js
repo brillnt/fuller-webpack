9a292a58ce2faa39d7767a7ec3a99fb107c85f9f
Dennis Porter Jr <dennisporterjr@dennismars.local>
2025-01-21 11:24:30 -0500

    adding back base css styles

--- project/src/pages/index.js
+++ project/src/pages/index.js
@@ -1,6 +1,9 @@
+import '../modules/base/base.css';
+
 import { onReady } from '../utils/helpers.js';
 import MenuChangeOnScroll from '../modules/menu-change-onscroll.js';
 import ThreeDSlider from '../modules/three-d-slider.js';
+
 
 onReady(() => {
   new MenuChangeOnScroll('nav');
