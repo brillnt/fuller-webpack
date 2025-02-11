97210b56ebb47b27824cb008effcc0d519c257cb
Dennis Porter Jr <dennisporterjr@dennismars.local>
2025-02-11 14:54:21 -0500

    first full animation push

--- project/src/pages/product.js
+++ project/src/pages/product.js
@@ -1,7 +1,41 @@
-import { onReady } from '../utils/helpers.js';
+import { onReady, selectAll,  isMobile } from '../utils/helpers.js';
 import FullerAngleWatcher from '../modules/fuller-angle-watcher/fuller-angle-watcher.js';
+import TextAnimateSections from '../modules/text-animate-sections/text-animate-sections.js';
+import { gsap } from '../utils/animation.js';
 
 
 onReady(() => {
   new FullerAngleWatcher('.card, .two-path-block');
+  new TextAnimateSections(['first-reveal', 'second-reveal', 'third-reveal']);
+
+  const fullerCards = selectAll('.card-row .card');
+  const twoPathCards = selectAll('.two-path-blocks .two-path-block');
+
+  if (!isMobile()) {
+    console.log('not mobile');
+    gsap.set(fullerCards, { x: -20, opacity: 0 });
+    gsap.set(twoPathCards, { y: 20, opacity: 0 });
+
+    gsap.to(fullerCards, {
+      x: 0,
+      opacity: 1,
+      stagger: 0.2,
+      scrollTrigger: {
+        trigger: '.card-row',
+        start: 'top 50%',
+        toggleActions: 'play none none reverse',
+      },
+    });
+
+    gsap.to(twoPathCards, {
+      y: 0,
+      opacity: 1,
+      stagger: 0.2,
+      scrollTrigger: {
+        trigger: '.two-path-blocks',
+        start: 'top 50%',
+        toggleActions: 'play none none reverse',
+      },
+    });
+  }
 });
--- project/src/pages/systems.js
+++ project/src/pages/systems.js
@@ -1,8 +1,25 @@
-import { onReady } from '../utils/helpers.js';
+import { onReady, selectAll } from '../utils/helpers.js';
 import AnimateGridRows from '../modules/animate-grid-rows/animate-grid-rows.js';
+import TextAnimateSections from '../modules/text-animate-sections/text-animate-sections.js';
+import { gsap } from '../utils/animation.js';
 
 
 onReady(() => {
   new AnimateGridRows('spc-blocks-container', 'spc');
   new AnimateGridRows('cc-blocks-container', 'cc');
+  new TextAnimateSections(['first-reveal', 'second-reveal', 'third-reveal', 'fourth-reveal', 'fifth-reveal']);
+
+  const configuratorCards = selectAll('.config-blocks .config-block');
+
+  gsap.set(configuratorCards, { x: 20, opacity: 0 });
+  gsap.to(configuratorCards, {
+    x: 0,
+    opacity: 1,
+    stagger: 0.2,
+    scrollTrigger: {
+      trigger: '.config-blocks',
+      start: 'top 50%',
+      toggleActions: 'play none none reverse',
+    },
+  });
 });
