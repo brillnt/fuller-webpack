01272d73785ce2c6ffea7c279eac607ad68dbfaf
Dennis Porter Jr <dennisporterjr@dennismars.local>
2025-02-23 17:05:17 -0500

    removing fuller angles from the 3 pillars

--- project/src/pages/home.js
+++ project/src/pages/home.js
@@ -3,11 +3,9 @@ import SwipeMediaBlock from '../modules/swipe-media-block/swipe-media-block.js';
 import TextAnimateSections from '../modules/text-animate-sections/text-animate-sections.js';
 import ConnectingLine from '../modules/connecting-line/connecting-line.js';
 import ThreePillars from '../modules/three-pillars/three-pillars.js';
-import FullerAngleWatcher from '../modules/fuller-angle-watcher/fuller-angle-watcher.js';
 
 onReady(() => {
   const swiper = new SwipeMediaBlock('home-learn');
-  new FullerAngleWatcher('.bci-media');
   new ThreePillars('benefits-content');
   new TextAnimateSections([
     {
