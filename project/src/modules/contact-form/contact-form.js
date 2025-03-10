664e97ba22d2ac6fe730d56d08ac696f652529a5
Dennis Porter Jr <dennisporterjr@dennismars.local>
2025-03-10 12:58:14 -0400

    updates to contact form styles and hints for choosing products/home models

--- project/src/modules/contact-form/contact-form.js
+++ project/src/modules/contact-form/contact-form.js
@@ -1,4 +1,4 @@
-import { select, selectId, on } from '../../utils/helpers.js';
+import { selectId, isMobile, on } from '../../utils/helpers.js';
 import Base from '../base/base.js';
 
 export default class ContactForm extends Base {
@@ -9,6 +9,8 @@ export default class ContactForm extends Base {
 
     this.element = selectId(elementId);
     this.triggerSelect = this.element.querySelector('#fuller-Interest');
+    this.multiSelectElem = selectId('Home-Model-Interest')
+    this.multiSelectLabel = this.element.querySelector('#multi-select-label');
     this.multiSelectContainer = this.element.querySelector('#multi-select-container');
     this.triggerValue = 'Product Inquiry';
 
@@ -21,6 +23,12 @@ export default class ContactForm extends Base {
   }
 
   init() {
+    if (isMobile()) {
+      this.multiSelectLabel.textContent = 'Select models (one or more, tap to choose):';
+    } else {
+      this.multiSelectLabel.textContent = 'Select models (hold shift to choose multiple):';
+    }
+
     this.multiSelectContainer.style.display = 'none';
 
     on(this.triggerSelect, 'change', () => {
